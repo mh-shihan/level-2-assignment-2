1225d1f34a4b7565358bd6d58e2d57806f62a374
mh-shihan <mahmodul.no.1@gmail.com>
2025-07-07 05:46:33 +0600

    Refactor product controller to combine product retrieval and search functionality

--- src/app/modules/product/product.controller.ts
+++ src/app/modules/product/product.controller.ts
@@ -27,7 +27,10 @@ const createProduct = async (req: Request, res: Response) => {
   }
 };
 
-const getAllProducts = async (req: Request, res: Response) => {
+const getAllProductsOrGetSearchedProduct = async (
+  req: Request,
+  res: Response,
+) => {
   try {
     const searchTerm = req.query.searchTerm?.toString() || '';
     if (searchTerm) {
@@ -130,33 +133,10 @@ const deleteProduct = async (req: Request, res: Response) => {
   }
 };
 
-const searchProduct = async (req: Request, res: Response) => {
-  try {
-    const searchTerm = req.query;
-    console.log(searchTerm);
-
-    // if (result.deletedCount > 0) {
-    //   res.status(200).json({
-    //     success: true,
-    //     message: "Products matching search term 'iphone' fetched successfully!",
-    //     data: null,
-    //   });
-    // }
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  } catch (error: any) {
-    res.status(500).json({
-      success: false,
-      message: error.message || 'Something went wrong',
-      error: error,
-    });
-  }
-};
-
 export const productController = {
   createProduct,
-  getAllProducts,
+  getAllProductsOrGetSearchedProduct,
   getSpecificProduct,
   updateProduct,
   deleteProduct,
-  searchProduct,
 };
--- src/app/modules/product/product.route.ts
+++ src/app/modules/product/product.route.ts
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.post('/', productController.createProduct);
 
-router.get('/', productController.getAllProducts);
+router.get('/', productController.getAllProductsOrGetSearchedProduct);
 
 router.get('/:productId', productController.getSpecificProduct);
 
