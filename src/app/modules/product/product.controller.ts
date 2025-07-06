d6606c6c16d71c72813c65d6de5e30f3f714cdd8
mh-shihan <mahmodul.no.1@gmail.com>
2025-07-06 20:14:31 +0600

    Add product retrieval functionality and validation in the product controller

--- src/app/modules/product/product.controller.ts
+++ src/app/modules/product/product.controller.ts
@@ -1,10 +1,14 @@
 import { Request, Response } from 'express';
 import { ProductServices } from './product.service';
+import { productValidationSchema } from './product.validation';
 
 const createProduct = async (req: Request, res: Response) => {
   try {
     const { product: productData } = req.body;
-    const result = await ProductServices.createProductIntoDB(productData);
+
+    const validatedProduct = productValidationSchema.parse(productData);
+
+    const result = await ProductServices.createProductIntoDB(validatedProduct);
 
     if (result) {
       res.status(200).json({
@@ -13,7 +17,26 @@ const createProduct = async (req: Request, res: Response) => {
         data: result,
       });
     }
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message || 'Something went wrong',
+      error: error,
+    });
+  }
+};
 
+const getAllProducts = async (req: Request, res: Response) => {
+  try {
+    const result = await ProductServices.getAllProductsFromDB();
+    if (result) {
+      res.status(200).json({
+        success: true,
+        message: 'Products fetched successfully!',
+        data: result,
+      });
+    }
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
   } catch (error: any) {
     res.status(500).json({
@@ -26,4 +49,5 @@ const createProduct = async (req: Request, res: Response) => {
 
 export const productController = {
   createProduct,
+  getAllProducts,
 };
--- src/app/modules/product/product.route.ts
+++ src/app/modules/product/product.route.ts
@@ -5,4 +5,6 @@ const router = express.Router();
 
 router.post('/', productController.createProduct);
 
+router.get('/', productController.getAllProducts);
+
 export const ProductRoute = router;
